6decffdab309ad188903ca18b0572386e776d2d2
japsasir <japsasir@gmail.com>
2021-10-31 19:01:30 +0100

    Automatización instalación sitio WP con CLI

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 set -x
 
-## El script debe utilizarse desde la ubicación del archivo 'docker-compose.yml' ##
+###-------------------------------------------------------###
+### Script para montar un sitio LMS con Wordpress         ###
+###-------------------------------------------------------###
+
+## Variables ##
+# IP pública o local (Pruebas)
+IP=192.168.0.41
+DB_NAME=wp_db
+DB_USER=wp_user
+DB_PASSWORD=wp_pass
+
+# Nos dirigimos al directorio de instalación en caso de no estarlo
+cd LMS-WP
+
 # Actualizamos 
 apt update
 
@@ -14,6 +27,20 @@ systemctl start docker
 
 # Levantamos los servicios. 
 docker-compose up -d 
+echo "Configurando el sitio Wordpress"
+
+# Asignamos un alias para lanzar comandos al contenedor wp-cli de manera ágil
+alias wp="sudo docker-compose exec wordpress wp"
+
+# Descargamos Wordpress
+wp core download --path=/var/www/html --locale=es_ES --allow-root
+
+# Creamos nuestro sitio LMS 'Beaver LMS'
+wp core install --url=$IP --title="Tu sitio LMS" --admin_user=admin --admin_password=admin_password --admin_email=test@test.com--allow-root
+
+# Configuramos el archivo de configuración de Wordpress.
+wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --allow-root
+
 
 ## Notas de docker-compose ##
 
