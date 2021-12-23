05aa1b793d6cef0beff7c298daf2f658ec599cbf
japsasir <japsasir@gmail.com>
2021-12-23 12:18:16 +0100

    Wordpress y CLI
    Falla el orden de carga aún con el script

--- alias.sh
+++
@@ -1,9 +0,0 @@
-## Test del subsistema de alias para poder automatizar comandos adicionales de wordpress. [experimental, no en uso]
-
-# Activamos la expansión de generar alias para bash, normalmente desactivada
-shopt -s expand_aliases
-
-# Test de funcionamiento de alias
-alias testme="echo It Worked"
-alias
-testme
